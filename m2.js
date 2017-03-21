e92bfb2ecca45e0d044930acefaf585389bf3b0a
hemantmiddha <middhahemant98@gmail.com>
2017-03-21 15:15:40 +0530

    Update m2.js

--- m2.js
+++ m2.js
@@ -1,7 +1,13 @@
 function sayHello () {
   var name =
    document.getElementById("name").value;
+  if(name==Hemant")
    var message = "<h2>Marks of " + name + " <br> Operating system = 10 <br> Web Programming = 09 <br> Maths = 11 " + name + "!</h2>";
+ else if(name=="jaiyesh")
+    var message = "<h2>Marks of " + name + " <br> Basic Electronics= 13 <br> Programming in c= 11 <br> physics = 11 " + name + "!</h2>";
+  else
+    var message  = "<h2>Marks of " + name + " Not Avilable </h2>";
+ 
 
   // document
   //   .getElementById("content")
