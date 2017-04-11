0f3ac15fbb0ef1de03825124d9b70d0249ca4d7e
hemantmiddha <middhahemant98@gmail.com>
2017-04-11 15:15:47 +0530

    Update m3script.js

--- m3script.js
+++ m3script.js
@@ -1,29 +1,29 @@
- SCRIPT.JS
-
-
-function onLoad(){
-	var name = document.getElementById("name").value;
-	// console.log(name);
-	var xhttp = new XMLHttpRequest;
+function loadData(){
+	var xhttp = new XMLHttpRequest();
 	xhttp.onreadystatechange = function(){
-		if (this.readyState == 4 && this.status ==200 ) {
-			var jsonObj = JSON.parse(xhttp.responseText);
-			var flag = 1;
-			// console.log(jsonObj.length);
-			for (var i = 0; i < jsonObj.length; i++) {
-				if (jsonObj[i].name == name) {
-					
-					break;
-				}else{
-					flag = 0;
-				}
-				if (flag == 0) {
-					document.getElementById("demo").innerHTML = "Incorrect Name";	
-				}
-			}
+		if(this.readyState == 4 && this.status==200){
+			var jsonObj = JSON.parse(this.responseText);
+			responseHandler(jsonObj);
 		}
 	};
+	function responseHandler(jsonObj){
+		var i = 0;
+		var number = document.getElementById('numberBox').value;
+		var output = document.getElementById('output');
+		var flag = 0;
+		// console.log(number);
+		for (var i = 0; i < jsonObj.length; i++) {
+			if (number == jsonObj[i].number) {
+				output.innerHTML = jsonObj[i].name;
+				flag = 1;
+				break;
+			}
+		}
+		if (flag == 0) {
+			output.innerHTML = "<span style='color:red;'>Number could not be found</span>";
+		}
+	}
 	xhttp.open("GET","data.json",true);
-	xhttp.send(null);
+	xhttp.send();	
 }
 
